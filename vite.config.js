3cfb22273ff997d320ff26e6fd1bff40b93ff6d3
anupkumarchakra <anupkumarchakra30@gmail.com>
2025-01-31 16:15:39 +0530

    updated build

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  // base: "./",
   optimizeDeps: {
     include: ["date-fns"],
   },
   build: {
     outDir: "dist",
-    assetsDir: "assets",
+    // assetsDir: "assets",
   },
 });
